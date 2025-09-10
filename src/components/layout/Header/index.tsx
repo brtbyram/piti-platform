90bf6d1e84c928f0e1c2210b480762c63af40437
brtbyram <brtbyram.61@gmail.com>
2025-09-10 16:13:28 +0300

    fix: Update Header and Sidebar styles for improved UI consistency

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -14,7 +14,7 @@ function Header() {
   const isTrainersPage = pathname.startsWith("/trainers");
 
   return (
-    <div className="bg-[var(--color-background)] sticky top-0 left-0 w-full z-10">
+    <div className="bg-black text-white sticky top-0 left-0 w-full z-10">
       <div className="h-20 container mx-auto justify-between flex items-center space-x-4">
         <Link href="/" className="text-4xl font-bold cursor-pointer">
           yourTrainer
@@ -23,12 +23,12 @@ function Header() {
         {/* search input */}
 
         {isTrainersPage && (
-          <div className="flex items-center w-5/12">
+          <div className="flex items-center w-5/12 text-white/40">
             <Icon name="search" size={24} />
             <input
               type="text"
               placeholder="Aradığınız antrenörü bulun..."
-              className="w-7/12 h-10 px-4 border-b-2 focus:border-b-1 border-white  focus:outline-none "
+              className="w-7/12 h-10 px-4 border-b-1 focus:border-b-1 border-white/40  focus:outline-none "
             />
           </div>
         )}
